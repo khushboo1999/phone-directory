de0c753ea4628b73ba6ee7e9b68571130ff4e830
khushboo <khushboojindal100@gmail.com>
2019-12-06 10:12:14 +0530

    Components: Created 'Header' Component

--- src/App.js
+++ src/App.js
@@ -3,17 +3,14 @@ import React from 'react';
 class App extends Component {
   render() {
   return (
-   <div>
-          <div className="header">
-          Phone Directory
-        </div>
-        <button>Add</button>
-        <div>
-          <span>Name</span><br />
-          <span>Phone</span>
-        </div>
-      </div>
-  );
+    <Header/>
+    <button>Add</button>
+    <div>
+      <span>Name</span><br />
+      <span>Phone</span>
+    </div>
+  </div>
+);
 }
-
+}
 export default App;
